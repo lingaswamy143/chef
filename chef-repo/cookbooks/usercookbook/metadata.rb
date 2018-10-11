f494aa1de39ed507443d44ace07a8ad8b8bb948c
lingaswamy143 <lingaswamy2764@gmail.com>
2018-10-11 14:35:59 +0530

    chef second

--- chef-repo/cookbooks/usercookbook/metadata.rb
+++ chef-repo/cookbooks/usercookbook/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures usercookbook'
 long_description 'Installs/Configures usercookbook'
-version '7.0'
+version '8.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
